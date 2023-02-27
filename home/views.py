a9dce3203ec40e38d8bb0b04077f392c0e09adb5
André Coêlho <andrelucascoelho281@gmail.com>
2023-02-27 10:16:07 -0300

    atualização

--- home/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def home(request):
-    return render(request, 'home.html', {})
-
-def produtos(request):
-    return HttpResponse('Estou na tela de projetos :)')
