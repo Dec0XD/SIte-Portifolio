53cf3e846ef8926b138508aa97814d72b92ce229
André Coêlho <andrelucascoelho281@gmail.com>
2023-03-01 08:57:23 -0300

    ajeitada no js

--- js/home.js
+++ js/home.js
@@ -1,29 +1,25 @@
-// Seleciona o botão de abertura do modal
+// abertura do modal
 const abrirModal = document.querySelector('.abrir-modal');
 // Seleciona o modal
 const modal = document.querySelector('.modal-codigo');
-// Seleciona o botão de fechamento do modal
+// botão de fechamento do modal
 const fecharModal = document.querySelector('.fechar-modal');
 
-// Adiciona um ouvinte de evento de clique no botão de abertura do modal
+// abertura do modal
 abrirModal.addEventListener('click', () => {
-  // Abre o modal adicionando a classe "aberto"
   modal.classList.add('aberto');
 });
 
-// Adiciona um ouvinte de evento de clique no botão de fechamento do modal
+// fechamento do modal
 fecharModal.addEventListener('click', () => {
-  // Fecha o modal removendo a classe "aberto"
   modal.classList.remove('aberto');
 });
 
 
 
-// define a função para mudar o idioma
+//função para mudar o idioma
 function changeLanguage(lang) {
-  // selecione todos os elementos com o atributo "data-translate"
   var elements = document.querySelectorAll("[data-translate]");
-  // itere sobre cada elemento e mude o texto de acordo com o idioma selecionado
   for (var i = 0; i < elements.length; i++) {
     var key = elements[i].getAttribute("data-translate");
     elements[i].innerHTML = translations[lang][key];
